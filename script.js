a9f27f106bc8c4bc288089e8609416e04a0535be
shpr <roman.shpr@gmail.com>
2022-02-20 06:59:31 +0200

    Apply prettier formatting

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
+const slider = document.getElementById('myRange');
+const output = document.getElementById('demo');
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+slider.oninput = function () {
+	output.innerHTML = this.value;
+};
